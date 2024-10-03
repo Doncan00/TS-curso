1d2a4bd80299e877b1a2a0ca13cec37fe937e034
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2024-10-03 13:28:57 -0600

    Elimino un proyecto

--- 01-react-foundation/src/Actividades/index.ts
+++
@@ -1,5 +0,0 @@
-
-
-export * from "./Greeting";
-export * from "./Contador";
-
--- 01-react-foundation/src/components/Index.ts
+++
@@ -1,6 +0,0 @@
-
-
-export * from "./Counter";
-export * from "./CounterWithHook";
-export * from "./LoginPage";
-export * from "./UserPage";
